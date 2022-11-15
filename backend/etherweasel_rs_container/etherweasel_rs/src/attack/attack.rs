6f9baa67d75425bf8553528bded5a210523377ea
jarpoole <jaredpoole123@gmail.com>
2022-11-15 18:56:49 -0500

    save

--- backend/etherweasel_rs_container/etherweasel_rs/src/attack/attack.rs
+++ backend/etherweasel_rs_container/etherweasel_rs/src/attack/attack.rs
@@ -2,6 +2,6 @@ use erased_serde::Serialize as ErasedSerialize;
 
 pub trait Attack {
     fn get_logs(&self) -> Vec<Box<dyn ErasedSerialize>>;
-    //fn stop() -> ();
-    //fn start(concurrency: u16) -> ();
+    fn start(&mut self) -> ();
+    fn stop(&mut self) -> ();
 }
